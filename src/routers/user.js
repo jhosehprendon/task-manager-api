9a02d56b4ca0390dca649ae0c561b00287318d78
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-04-16 08:55:13 -0500

    Created Login route

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,6 +27,16 @@ router.post('/users', async (req, res) => {
 
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch(e) {
+        res.status(400).send()
+    }
+})
+
+
 router.get('/users', async (req, res) => {
 
     try {
@@ -78,7 +88,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((el) => user[el] = req.body[el])
+
+        await user.save()
+
         if(!user) {
             return res.status(404).send()
         }
