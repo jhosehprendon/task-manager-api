fe5ea198d4f49605d156718168d3adcc40aa9dc9
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-04-17 09:34:59 -0500

    Filtering tasks by query params

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,10 +28,24 @@ router.post('/tasks', auth, async (req, res) => {
     // })
 })
 
+
+// GET /tasks?completed=false
+
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if(req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        // const tasks = await Task.find({ owner: req.user._id })
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        // res.send(tasks)
+        res.send(req.user.tasks)
     } catch(e) {
         res.status(500).send()
     }
