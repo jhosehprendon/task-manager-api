940ca36588336dca021eda86a8c6a3db4b5090cb
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-04-17 13:26:42 -0500

    Normalizing image dimensions and format

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
-const multer = require('multer');
 
 const router = new express.Router();
 
@@ -160,7 +161,10 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    // req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -183,7 +187,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
 
     } catch(e) {
