e17a500cd80ef7268eb9a4a7001b9e0f2625824c
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-04-18 10:28:45 -0500

    Integrated sendgrid and send Welcome and cancelation emails

---
+++ src/emails/account.js
@@ -0,0 +1,30 @@
+const sgMail = require('@sendgrid/mail');
+
+const sendgridAPIKey = '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';
+
+sgMail.setApiKey(sendgridAPIKey);
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'pay.jhosehp@gmail.com',
+        subject: 'Welcome to Task Manager',
+        text: `Hi ${name}, Welcome to Task Manager app. with this new tool you can create you daily tasks and manage them in one place. Hope you enjoy it and do not hesitate to contact us f you have any question or problem.`
+    })
+
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'pay.jhosehp@gmail.com',
+        subject: 'Sorry to see you go',
+        text: `Hi ${name}, we are sorry to see you go but you can come back whenever you want :)`
+    })
+
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
